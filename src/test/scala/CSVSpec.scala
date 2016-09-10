cbc0186a4f88407043589f4138907624a20d14a4
Bernardo Uriarte <berublan@gmail.com>
2016-09-10 18:23:31 +0200

    Add tests for CSV parsing

---
+++ src/test/scala/CSVSpec.scala
@@ -0,0 +1,23 @@
+import org.scalatest.FunSuite
+import CSV._
+
+class CSVSpec extends FunSuite {
+
+  def check(actual: Seq[String], expected: Seq[String]): Unit = {
+    assert(actual == expected, s"$actual did not equal $expected")
+  }
+
+  test("Line without delimiters should return sigle element") {
+    check(parseLine("foo", ','), Seq("foo"))
+  }
+
+  test("Should no split quoted delimiters") {
+    val res = parseLine("\"foo|bar|baz\"|foo|bar|baz", '|')
+    check(res, Seq("foo|bar|baz", "foo", "bar", "baz"))
+  }
+
+  test("Should interpret ecaped quotes") {
+    val res = parseLine("\"some \"\"quoted\"\" text\",\"\"\"\",123", ',')
+    check(res, Seq("some \"quoted\" text", "\"", "123"))
+  }
+}
