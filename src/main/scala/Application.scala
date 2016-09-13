23bbd665712334e5f6c538069c6eaf4533d129cf
Bernardo Uriarte <berublan@gmail.com>
2016-09-13 21:57:17 +0200

    add ui package object with helper functions and show typeclass

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,6 +1,5 @@
-import model._
 import services._
-import util._
+import ui._
 
 object Application {
 
@@ -10,58 +9,29 @@ object Application {
     loop()
   }
 
-  def chooseFrom[T](first: T, rest: T*): T = chooseFrom(first +: rest)
-
-  def chooseFrom[T](source: Seq[T]): T = {
-    for ((el, i) <- source.zipWithIndex) {
-      println(s"$i) $el")
-    }
-    var selection: Int = -1
-    while (!source.indices.contains(selection)) {
-      print("Selection: ")
-      selection = io.StdIn.readInt()
-    }
-    source(selection)
-  }
-
-  sealed trait Action
-
-  object SearchByCode extends Action {
-    override def toString = "Search by code"
-  }
-
-  object SearchByName extends Action {
-    override def toString = "Search by name"
-  }
-
-  object Exit extends Action {
-    override def toString = "Exit"
-  }
-
   def loop(): Unit = {
-
-    chooseFrom(SearchByCode, SearchByName, Exit) match {
-      case SearchByCode =>
-        println("Enter country code:")
+    chooseFrom(EnterCode, SearchByName, Exit) match {
+      case EnterCode =>
+        showConsole("Enter country code:")
         val code = io.StdIn.readLine()
         svc.getCountryByCode(code) match {
           case Some(country) =>
             val res = svc.getAirportsAndRunwaysByCountry(country)
-            println(res)
-          case None => println("Country not found")
+            showConsole(res)
+          case None => showConsole("Country not found")
         }
       case SearchByName =>
-        println("Enter country name:")
+        showConsole("Enter country name:")
         val name = io.StdIn.readLine()
         svc.findCountriesByName(name) match {
-          case cs if cs.isEmpty => println("No countries found")
+          case cs if cs.isEmpty => showConsole("No countries found")
           case Seq(country) =>
             val res = svc.getAirportsAndRunwaysByCountry(country)
-            println(res)
+            showConsole(res)
           case cs =>
             val country = chooseFrom(cs)
             val res = svc.getAirportsAndRunwaysByCountry(country)
-            println(res)
+            showConsole(res)
         }
       case Exit => return
     }
---
+++ src/main/scala/ui.scala
@@ -0,0 +1,87 @@
+import model._
+
+import scala.util.Try
+
+package object ui {
+
+  trait Show[T] {
+    def show(t: T): String
+  }
+
+  implicit val countryShow = new Show[Country] {
+    def show(country: Country): String = s"${country.code} - ${country.name} (${country.continent})"
+  }
+
+  implicit val airportShow = new Show[Airport] {
+    def show(airport: Airport): String = airport.name
+  }
+
+  implicit val runwayShow = new Show[Runway] {
+    def show(runway: Runway): String = {
+      val dim = Seq(
+        runway.widthFt.map("width: " + _.toString + " ft"),
+        runway.lengthFt.map("length: " + _.toString + " ft")
+      ).flatten.mkString(", ")
+      if (dim.isEmpty)
+        runway.id.toString
+      else
+        runway.id + " - " + dim
+    }
+  }
+
+  implicit val resultShow = new Show[(Airport, Seq[Runway])] {
+    def show(res: (Airport, Seq[Runway])): String = {
+      val (airport, runways) = res
+      val sb = new StringBuilder
+      sb.append("-" + airportShow.show(airport) + "\n")
+      if (runways.isEmpty)
+        sb.append("\tNo runways\n")
+      else {
+        sb.append("\tRunways: \n")
+        for (r <- runways) {
+          sb.append("\t-" + runwayShow.show(r) + "\n")
+        }
+      }
+      sb.toString()
+    }
+  }
+
+  implicit def seqShow[T](implicit st: Show[T]) = new Show[Seq[T]] {
+    def show(seq: Seq[T]): String =
+      seq.map(st.show).mkString("\n")
+  }
+
+  implicit def genShow[T] = new Show[T] {
+    def show(t: T): String = t.toString
+  }
+
+
+  sealed trait Action
+  object EnterCode extends Action
+  object SearchByName extends Action
+  object Exit extends Action
+
+  implicit val actionShow = new Show[Action] {
+    def show(action: Action): String = action match {
+      case SearchByName => "Search by name"
+      case EnterCode => "Enter code"
+      case Exit => "Exit"
+    }
+  }
+
+  def chooseFrom[T](first: T, rest: T*)(implicit st: Show[T]): T = chooseFrom(first +: rest)(st)
+
+  def chooseFrom[T](source: Seq[T])(implicit st: Show[T]): T = {
+    for ((el, i) <- source.zipWithIndex) {
+      println(s"$i) ${st.show(el)}:")
+    }
+    var selection: Int = -1
+    while (!source.indices.contains(selection)) {
+      print("Selection: ")
+      selection = Try(io.StdIn.readLine().toInt).getOrElse(-1)
+    }
+    source(selection)
+  }
+
+  def showConsole[T](t: T)(implicit st: Show[T]) = println(st.show(t))
+}
