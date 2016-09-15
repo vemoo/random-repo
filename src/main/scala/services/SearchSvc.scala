824b95ab207a3e03b9af1584ace760a8faf2db1f
Bernardo Uriarte <berublan@gmail.com>
2016-09-15 19:17:55 +0200

    Documentation

--- src/main/scala/services/SearchSvc.scala
+++ src/main/scala/services/SearchSvc.scala
@@ -17,16 +17,31 @@ trait SearchSvc extends DAO {
   private val airportsByCountryCode =
     getAirports.map(_.groupBy(_.isoCountry).withDefaultValue(Seq()))
 
+  /**
+    * Finds countries begining by name
+    * @param name Name or begining of the name of the country, case insensitive
+    * @return
+    */
   def findCountriesByName(name: String): Future[Seq[Country]] = {
     val nameUpper = name.toUpperCase()
     countries.map(_.filter(_.name.toUpperCase().startsWith(nameUpper)))
   }
 
+  /**
+    * Finds countries by code exactly
+    * @param code ISO country code, case insensitive
+    * @return
+    */
   def getCountryByCode(code: String): Future[Option[Country]] = {
     val codeUpper = code.toUpperCase()
     countries.map(_.find(_.code.toUpperCase() == codeUpper))
   }
 
+  /**
+    * Get the airports and its runways for a five country
+    * @param country
+    * @return
+    */
   def getAirportsAndRunwaysByCountry(country: Country): Future[Seq[Result]] = {
     for {
       aByCC <- airportsByCountryCode
